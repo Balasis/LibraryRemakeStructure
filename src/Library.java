8939419524af7bfc4b7831455284934f93be4490
Balasis <giovani1994a@gmail.com>
2024-03-29 19:45:33 +0200

    RebuildingTheStructure

--- src/Library.java
+++ src/Library.java
@@ -2,11 +2,14 @@ import java.util.ArrayList;
 
 public class Library {
 
+    private ArrayList<Borrowable> borrowables;
     private ArrayList<Member> members;
+    private ArrayList<Title> titles;
 
-    public Library(){
+    public Library(ArrayList<Borrowable> borrowables , ArrayList<Title> titles){
         members=new ArrayList<Member>();
-
+        this.borrowables=borrowables;
+        this.titles=titles;
     }
 
 }
--- src/LibraryProgram.java
+++ src/LibraryProgram.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class LibraryProgram {
     public static void main(String[] args) {
-
+        ArrayList<Borrowable> borrowables=new ArrayList<Borrowable>();
+        ArrayList<Title> titles=new ArrayList<Title>();
+        Library library=new Library(borrowables,titles);
     }
 }
